67845972226299e2d30ebb84cd845d39d319ab86
xxtractz <musambaloyi@gmail.com>
2019-08-11 23:15:59 +0200

    update on Front end page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,93 +4,88 @@
 $this->start('body'); ?>
 
 <?php if (FH::getUser()) {?>
-    <div class="row">
-        <div class="col-4 mx-auto pt-lg-5">
-            <div class="jumbotron text-center p-5">
-                <h3 class="text-center">Select your duration of activity</h3>
-                <hr>
-                <form class="form" action="" method="post">
-                    <?= FH::csrfInput() ?>
-                    <div class="row">
-                        <div class="col">
-                            <!-- Group of default radios - option 1 -->
-                            <div class="ml-auto form-check custom-radio">
-                                <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios">
-                                <label class="custom-control-label" for="defaultGroupExample1">1 - Week </label>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <!-- Group of default radios - option 2 -->
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios" checked>
-                                <label class="custom-control-label" for="defaultGroupExample2">2 - Weeks</label>
-                            </div>
-                            <!-- Group of default radios - option 3 -->
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" id="defaultGroupExample3" name="groupOfDefaultRadios">
-                                <label class="custom-control-label" for="defaultGroupExample3">3 - Weeks</label>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <!-- Group of default radios - option 4 -->
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" id="defaultGroupExample4" name="groupOfDefaultRadios">
-                                <label class="custom-control-label" for="defaultGroupExample4">4 - Weeks</label>
-                            </div>
-                            <!-- Group of default radios - option 8 -->
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" id="defaultGroupExample8" name="groupOfDefaultRadios">
-                                <label class="custom-control-label" for="defaultGroupExample8">8 - Weeks</label>
-                            </div>
-                        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="mx-auto pt-lg-5">
+                    <div class="jumbotron text-center p-1">
+                        <h3 class="text-center text-black-50">Your Membership</h3>
                     </div>
-                </form>
+                    <div class="col-sm jumbotron p-4">
+                        <form class="form" action="" method="post">
+                            <?= FH::csrfInput() ?>
+                            <div class="form-group">
+                                <label for="activity">Please select how long you want to be active for</label>
+                                <select multiple class="form-control">
+                                    <option>1 - Week</option>
+                                    <option>2 - Weeks</option>
+                                    <option>3 - Weeks</option>
+                                    <option>4 - Weeks</option>
+                                    <option>8 - Weeks</option>
+                                </select>
+                                <div class="d-flex justify-content-end mt-2">
+                                    <?= FH::submitTag('Subscribe',['class'=>'btn btn-primary']) ?>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-6 mx-auto pt-lg-5 ">
-            <div class="jumbotron text-center p-5 ">
-                <h3 class="text-center">Downliners</h3>
-                <hr>
-                <div class="row mh-50">
-                    <div class="col">
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                            <tr>
-                                <th class="th-lg h5-responsive table-success">
-                                    Active Downliners
-                                </th>
-                            </tr>
-                            </thead>
-                            <tr>
-                                <td>KATLEGO MATHABELA</td>
-                            </tr>
-                            <tr>
-                                <td>JACOB MAEPA</td>
-                            </tr>
-                            <tr>
-                                <td>GONTSE SWARATLHE</td>
-                            </tr>
-                        </table>
+            <div class="col-sm-8">
+                <div class="mx-auto pt-lg-5 ">
+                    <div class="jumbotron text-center p-1">
+                        <h3 class="text-center text-black-50">Your Downliners</h3>
                     </div>
-                    <div class="col">
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                            <tr>
-                                <th class="th-lg h5-responsive table-danger">
-                                    Inactive Downliners
-                                </th>
-                            </tr>
-                            </thead>
-                            <tr>
-                                <td>KATLEGO MATHABELA</td>
-                            </tr>
-                            <tr>
-                                <td>JACOB MAEPA</td>
-                            </tr>
-                            <tr>
-                                <td>GONTSE SWARATLHE</td>
-                            </tr>
-                        </table>
+                    <div class="row">
+                        <div class="col-sm-5 mx-auto">
+                            <div style="height: 300px;overflow: auto" class="shadow-lg">
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th class="th-lg h5-responsive table-success sticky-top mb-0">
+                                            Active Downliners
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tr>
+                                        <td>KATLEGO MATHABELA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>JACOB MAEPA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>JACOB MAEPA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>GONTSE SWARATLHE</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="col-sm-5 mx-auto">
+                            <div style="height: 300px;overflow: auto" class="shadow-lg">
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th class="th-lg h5-responsive table-danger sticky-top">
+                                            Inactive Downliners
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tr>
+                                        <td>KATLEGO MATHABELA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>JACOB MAEPA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>GONTSE SWARATLHE</td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
